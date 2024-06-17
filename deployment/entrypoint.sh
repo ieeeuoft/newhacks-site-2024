920ed1e2f2624535a9cda9491f5adbfee8c29cee
Ruihao Li <a834878215@gmail.com>
2024-06-17 14:20:02 -0400

    Deployment setup
    Deployment setup

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -2,10 +2,20 @@
 
 echo "Waiting for the database..."
 
-while ! pg_isready -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER; do
+TIMEOUT=120
+ATTEMPT=0
+command="pg_isready -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER"
+
+until [ "$ATTEMPT" -eq "$TIMEOUT" ] || eval $command; do
+  (( ATTEMPT++ ))
   sleep 1
 done
 
-echo "Database connection made"
-
-exec "$@"
+if [ "$ATTEMPT" -lt "$TIMEOUT" ]
+then
+  echo "Database connection made"
+  exec "$@"
+else
+  echo "Failed to connect to database"
+  exit 1
+fi
