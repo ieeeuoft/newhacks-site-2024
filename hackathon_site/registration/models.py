d178501d05dc290c40ab079f03bfac2facad054b
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2024-08-26 22:05:22 -0400

    migration changes

--- hackathon_site/registration/models.py
+++ hackathon_site/registration/models.py
@@ -139,21 +139,21 @@ class Application(models.Model):
     devpost = models.URLField(
         max_length=200, help_text="Devpost Profile (Optional)", null=True, blank=True
     )
-    # why_participate = models.TextField(
-    #     null=False,
-    #     help_text="Why do you want to participate in NewHacks?",
-    #     max_length=1000,
-    # )
-    # what_technical_experience = models.TextField(
-    #     null=False,
-    #     help_text="What is your technical experience with software?",
-    #     max_length=1000,
-    # )
-    # what_past_experience = models.TextField(
-    #     null=False,
-    #     help_text="If you’ve been to a hackathon, briefly tell us your experience. If not, describe what you expect to see and experience.",
-    #     max_length=1000,
-    # )
+    why_participate = models.TextField(
+        null=True,
+        help_text="Why do you want to participate in NewHacks?",
+        max_length=1000,
+    )
+    what_technical_experience = models.TextField(
+        null=True,
+        help_text="What is your technical experience with software?",
+        max_length=1000,
+    )
+    what_past_experience = models.TextField(
+        null=True,
+        help_text="If you’ve been to a hackathon, briefly tell us your experience. If not, describe what you expect to see and experience.",
+        max_length=1000,
+    )
     conduct_agree = models.BooleanField(
         help_text="I have read and agree to the "
         '<a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" rel="noopener noreferrer" target="_blank">MLH code of conduct</a>.',
