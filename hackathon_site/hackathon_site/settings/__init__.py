6b845fe0cea54790caadffa26ca22da45cf2245a
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2024-08-29 15:14:33 -0400

    changed registration closing date to oct 11

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -317,7 +317,7 @@ CONTACT_EMAIL = DEFAULT_FROM_EMAIL
 HSS_ADMIN_EMAIL = "hardware@newhacks.ca"
 
 REGISTRATION_OPEN_DATE = datetime(2024, 1, 18, 0, 0, 0, tzinfo=TZ_INFO)
-REGISTRATION_CLOSE_DATE = datetime(2024, 10, 10, 23, 59, 0, tzinfo=TZ_INFO)
+REGISTRATION_CLOSE_DATE = datetime(2024, 10, 11, 23, 59, 0, tzinfo=TZ_INFO)
 APPLICATION_OPEN_DATE = datetime(2025, 10, 1, 0, 0, 0, tzinfo=TZ_INFO)
 EVENT_START_DATE = datetime(2024, 10, 26, 8, 0, 0, tzinfo=TZ_INFO)
 EVENT_END_DATE = datetime(2024, 10, 27, 17, 0, 0, tzinfo=TZ_INFO)
