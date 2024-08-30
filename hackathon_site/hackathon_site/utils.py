5c0d3e9dda85413a8d58ada19a2e8cf8b1617713
Ruihao Li <a834878215@gmail.com>
2024-08-29 23:22:20 -0400

    Revert "Locking Applications (#9)"
    This reverts commit fd97aac4cd4b4fb33448a066d0cf7aaf20a9e6a0.

--- hackathon_site/hackathon_site/utils.py
+++ hackathon_site/hackathon_site/utils.py
@@ -5,9 +5,7 @@ from django.conf import settings
 
 def is_registration_open():
     """
-    Determine whether registration is currently open.
-
-    Note: Registration being open refers to a user being able to simply create an account.
+    Determine whether registration is currently open
     """
     if settings.IN_TESTING:
         # So tests don't rely on the date, default to true
@@ -17,22 +15,3 @@ def is_registration_open():
     # is configured to match TIME_ZONE. We then make the datetime object timezone-aware.
     now = datetime.now().replace(tzinfo=settings.TZ_INFO)
     return settings.REGISTRATION_OPEN_DATE <= now < settings.REGISTRATION_CLOSE_DATE
-
-
-def is_application_open():
-    """
-    Determine whether applications are currently open.
-
-    Note: Applications being open refers to a user's ability to fill out and
-    submit the hackathon application
-    The application time window is defined as being between the application open
-    date (inclusive), and when registration closes (exclusive)
-    """
-    if settings.IN_TESTING:
-        # So tests don't rely on the date, default to true
-        return True
-
-    # datetime.now() returns the system native time, so this assumes that the system timezone
-    # is configured to match TIME_ZONE. We then make the datetime object timezone-aware.
-    now = datetime.now().replace(tzinfo=settings.TZ_INFO)
-    return settings.APPLICATION_OPEN_DATE <= now < settings.REGISTRATION_CLOSE_DATE
