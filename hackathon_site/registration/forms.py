a6cfa80416bf710f6355a1654592f508d8dbd328
carmen-chau <carmenjchau@gmail.com>
2024-09-19 23:34:18 -0400

    black changes

--- hackathon_site/registration/forms.py
+++ hackathon_site/registration/forms.py
@@ -145,7 +145,7 @@ class ApplicationForm(forms.ModelForm):
             "linkedin",
             "github",
             "devpost",
-            "how_many_hackathons", # TODO: New Section
+            "how_many_hackathons",  # TODO: New Section
             "why_participate",
             "what_technical_experience",
             "what_past_experience",
